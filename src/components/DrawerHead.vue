a3a75dc871a00e385e8904b92ae508bad7fdecb5
Regina1997 <56130620+Regina1997@users.noreply.github.com>
2024-02-20 19:15:34 +0300

    first commit

---
+++ src/components/DrawerHead.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { inject } from 'vue'
+
+const { closeDrawer } = inject('cart')
+</script>
+
+<template>
+    <div class="flex items-center gap-5 mb-5">
+      <img @click="closeDrawer" src="/arrow-next.svg" alt="arrow" class="opacity-30 cursor-pointer rotate-180 hover:opacity-100 transition hover:translate-x-1" />
+      <h2 class="text-2xl font-bold">Cart</h2>
+    </div>
+</template>
---
+++ src/pages/Favorites.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      'https://88c71acfcd8e7d2b.mokky.dev/favorites?_relations=items'
+    )
+
+    favorites.value = data.map((obj) => ({
+      ...obj.item,
+      isFavorite: true,
+    }))
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8">Favorites</h2>
+
+  <CardList :items="favorites" is-favorites />
+</template>
